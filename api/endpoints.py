238d2497c6fe73847aed1dc08a95a0547a34910e
brokendev <fluccas09@gmail.com>
2025-06-13 14:52:31 -0300

    feat: added the new endpoints of API-gov

--- api/endpoints.py
+++ api/endpoints.py
@@ -10,3 +10,27 @@ def get_plano_acao_especial(params=None):
 
 def get_empenho_especial(params=None):
     return client.get("/empenho_especial", params)
+
+def get_documento_habil_especial(params=None):
+    return client.get("/documento_habil_especial", params)
+
+def get_ordem_pagamento_ordem_bancaria_especial(params=None):
+    return client.get("/ordem_pagamento_ordem_bancaria_especial", params)
+
+def get_historico_pagamento_especial(params=None):
+    return client.get("/historico_pagamento_especial", params)
+
+def get_relatorio_gestao_especial(params=None):
+    return client.get("/relatorio_gestao_especial", params)
+
+def get_meta_especial(params=None):
+    return client.get("/meta_especial", params)
+
+def get_executor_especial(params=None):
+    return client.get("/executor_especial", params)
+
+def get_plano_trabalho_especial(params=None):
+    return client.get("/plano_trabalho_especial", params)
+
+def get_finalidade_especial(params=None):
+    return client.get("/finalidade_especial", params)
